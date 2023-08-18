fc6c5c2fa3be64b0233356c84885605f75ca4f3d
trialUser543 <trialuser543@gmail.com>
2023-08-18 20:17:25 +0530

    Adding Python Scripts

---
+++ pythonScripts/triggerscripts.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import sys
+
+TOKEN = str(sys.argv[1])
+OWNER = str(sys.argv[2])
+REPO = str(sys.argv[3])
+Workflow_Name = str(sys.argv[4])
+parameter1 = str(sys.argv[5])
+parameter2 = str(sys.argv[6])
+
+print("The token value is")
+def trigger_workflow(Workflow_Name, parameter1, parameter2):
+
+	headers = {
+		"Accept": "application/vnd.github.v3+json",
+		"Authorization": f"token {TOKEN}",
+	}
+	
+	data = {
+		"event_type": Workflow_Name,
+		"client_payload": {
+			'parameter1': parameter1,
+			'parameter2': parameter2
+		}
+	}
+	
+	responseValue = requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
+	print(responseValue.content)
+	print("The response message is ", responsevalue.content)
+	
+trigger_workflow(Workflow_Name, parameter1, parameter2)
